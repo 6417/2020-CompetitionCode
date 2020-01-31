decb6ef7a6836f2c431455006eda37ba6aa552b0
Widizzi <florian@widi.ch>
2020-02-01 00:24:08 +0100

    Feature: Color Sensor reading

--- src/main/java/frc/robot/subsystems/ControlPanelSubsystem.java
+++ src/main/java/frc/robot/subsystems/ControlPanelSubsystem.java
@@ -8,10 +8,16 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorSensorV3;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
+import edu.wpi.first.wpilibj.smartdashboard.SendableRegistry;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Motors;
@@ -25,11 +31,35 @@ public class ControlPanelSubsystem extends SubsystemBase {
   private final DoubleSolenoid damperSolenoid = new DoubleSolenoid(Constants.PCM_CAN_ID,
       Constants.CONTROL_PANEL_DAMPING_SOLENOID_EXTEND_ID, Constants.CONTROL_PANEL_DAMPING_SOLENOID_RETRACT_ID);
 
+  private final ColorSensorV3 colorSensor = new ColorSensorV3(Constants.CONTROL_PANEL_COLOR_SENSOR_I2C_PORT);
+  /**
+   * A Rev Color Match object is used to register and detect known colors. This
+   * can be calibrated ahead of time or during operation.
+   * 
+   * This object uses a simple euclidian distance to estimate the closest match
+   * with given confidence range.
+   */
+  private final ColorMatch m_colorMatcher = new ColorMatch();
+
+  /**
+   * Note: Any example colors should be calibrated as the user needs, these are
+   * here as a basic example.
+   */
+  private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+  private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+  private final Color kRedTarget = ColorMatch.makeColor(0.43, 0.39, 0.16);
+  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+
   public enum ControlPanelMode {
     POSITION_CONTROL, ROTATION_CONTROL
   }
 
-  private Enum mode = ControlPanelMode.ROTATION_CONTROL;
+  public enum ColorDetected {
+    RED, GREEN, BLUE, YELLOW, NONE
+  }
+
+
+  private ControlPanelMode mode = ControlPanelMode.ROTATION_CONTROL;
 
   private String gameData;
 
@@ -39,12 +69,44 @@ public class ControlPanelSubsystem extends SubsystemBase {
    * Creates a new ControlPlanelSubsystem.
    */
   public ControlPanelSubsystem() {
-
+    addColorsToColorMatcher(kBlueTarget, kGreenTarget, kRedTarget, kYellowTarget);
+    m_colorMatcher.setConfidenceThreshold(0.95);
+    super.addChild("Lift Solenoid", liftSolenoid);
+    super.addChild("Damper Solenoid", damperSolenoid);
+    super.addChild("Control Panel Motor", Motors.control_panel_motor);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public void addColorsToColorMatcher(Color... colors) {
+    for(Color color : colors) {
+      m_colorMatcher.addColorMatch(color);
+    }
+  }
+
+  public ColorDetected getColor() {
+    ColorMatchResult match = m_colorMatcher.matchColor(colorSensor.getColor());
+    ColorDetected color = ColorDetected.NONE;
+    if (match == null) {
+      return ColorDetected.NONE;
+    } else if (match.color == kBlueTarget) {
+      color = ColorDetected.BLUE;
+    } else if (match.color == kRedTarget) {
+      color = ColorDetected.RED;
+    } else if (match.color == kGreenTarget) {
+      color = ColorDetected.GREEN;
+    } else if (match.color == kYellowTarget) {
+      color = ColorDetected.YELLOW;
+    }
+    return color;
+  }
+
+  public String getRGB() {
+    Color color = colorSensor.getColor();
+    return String.format("%f %f %f", color.red, color.green, color.blue);
   }
 
   void set(final DoubleSolenoid cylinder, final PneumaticState state) {
@@ -188,6 +250,14 @@ public class ControlPanelSubsystem extends SubsystemBase {
 
   public boolean getCancel() {
     return cancel;
+  }
+
+  @Override
+  public void initSendable(SendableBuilder builder) {
+    super.initSendable(builder);
+    builder.addStringProperty("Color", () -> getColor().toString(), null);
+    builder.addStringProperty("RGB", () -> getRGB(), null);
+    builder.addDoubleProperty("Motor", () -> Motors.control_panel_motor.get(), speed -> Motors.control_panel_motor.set(speed));
   }
 
 }
