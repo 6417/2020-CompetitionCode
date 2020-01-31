a58e64df4a12c03aa492bbe603fb14d4263b690c
Widizzi <florian@widi.ch>
2020-02-01 00:30:45 +0100

    Cleanup imports in Motors

--- src/main/java/frc/robot/Motors.java
+++ src/main/java/frc/robot/Motors.java
@@ -13,11 +13,8 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * Add your docs here.
